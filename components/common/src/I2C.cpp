d83a1ff44c7bd34aa41cb7ed7726be1ccda63554
yandvo <yandvo@gmail.com>
2025-10-26 14:18:43 -0400

    added i2c read and updated structure to be more aligned with Espressif inteded usage

--- components/common/src/I2C.cpp
+++ components/common/src/I2C.cpp
@@ -1,15 +1,18 @@
 #include "I2C.hpp"
 #include "driver/i2c_master.h"
+#include "driver/i2c_types.h"
 #include "esp_err.h"
 #include "esp_log.h"
+#include <cstddef>
 #include <cstring>
 
+/* Implementation */
 namespace Common {
 
 static const char *TAG = "I2C";
 
 // Contstructor
-I2C::I2C(i2c_port_t port, gpio_num_t sda, gpio_num_t scl) : port(port) {
+I2C::I2C(i2c_port_t port, gpio_num_t sda, gpio_num_t scl) {
   ESP_LOGI(TAG, "Initializing I2C on port %d (SDA=%d, SCL=%d)", port, sda, scl);
 
   // populate master cfg struct with mandatory parameters
@@ -27,14 +30,21 @@ I2C::I2C(i2c_port_t port, gpio_num_t sda, gpio_num_t scl) : port(port) {
 
 // Destructor
 I2C::~I2C() {
-  ESP_LOGI(TAG, "Deleting I2C driver on port %d", port);
+  ESP_LOGI(TAG, "Deleting I2C driver");
+
+  // remove devices
+  for (I2CDevice &dev : devices) {
+    i2c_master_bus_rm_device(dev.handle);
+  }
+
+  // remove master handle
   i2c_del_master_bus(this->bus_handle);
 }
 
-/* write
- *  - initiates an i2c write to device
+/* init_device
+ *  - registers device with master handler and adds to cache
  */
-esp_err_t I2C::write(uint8_t device_addr, uint8_t reg, const uint8_t *data, size_t len, int freq) {
+esp_err_t I2C::init_device(uint8_t device_addr, uint32_t freq) {
   // create device cfg
   i2c_device_config_t dev_cfg = {};
   dev_cfg.dev_addr_length = I2C_ADDR_BIT_LEN_7;
@@ -43,15 +53,46 @@ esp_err_t I2C::write(uint8_t device_addr, uint8_t reg, const uint8_t *data, size
 
   // get device handle and assign to master
   i2c_master_dev_handle_t dev_handle;
-  ESP_ERROR_CHECK(i2c_master_bus_add_device(bus_handle, &dev_cfg, &dev_handle));
+  esp_err_t ret = i2c_master_bus_add_device(bus_handle, &dev_cfg, &dev_handle);
+
+  if (ret == ESP_OK) {
+    // add device to cache
+    devices.push_back({device_addr, freq, dev_handle});
+  }
+
+  return ret;
+}
+
+/* get_device_handle
+ *  - retrieves device handle from cache
+ */
+i2c_master_dev_handle_t I2C::get_device_handle(uint8_t device_addr) {
+  for (I2CDevice &dev : devices) {
+    if (dev.address == device_addr)
+      return dev.handle;
+  }
+
+  // if no device found return null
+  return nullptr;
+}
+
+/* reg_write
+ *  - initiates an i2c write to device register
+ *  - Sends Address + Write, then Reg Address, then Data
+ */
+esp_err_t I2C::reg_write(uint8_t device_addr, uint8_t reg, const uint8_t *data, size_t len) {
+
+  // get device handle
+  i2c_master_dev_handle_t dev_handle = get_device_handle(device_addr);
 
   // prepare data
   i2c_master_transmit_multi_buffer_info_t tx_buffers[NUMBUFFERS] = {
-      {.write_buffer = &reg, .buffer_size = BYTELENGTH},
+      {.write_buffer = &reg, .buffer_size = sizeof(reg)},
       {.write_buffer = (uint8_t *)data, .buffer_size = len}};
 
   // transmit
-  esp_err_t ret = i2c_master_multi_buffer_transmit(dev_handle, tx_buffers, 2, -1);
+  esp_err_t ret =
+      i2c_master_multi_buffer_transmit(dev_handle, tx_buffers, NUMBUFFERS, I2CTIMEOUT_MS);
 
   // check for success
   if (ret != ESP_OK)
@@ -62,24 +103,23 @@ esp_err_t I2C::write(uint8_t device_addr, uint8_t reg, const uint8_t *data, size
   return ret;
 }
 
-/* read
+/* reg_read
  *  - initiates i2c read from device
+ *  - Sends Address + Write, then Register Address, then Reads to buffer
  */
-esp_err_t I2C::read(uint8_t device_addr, uint8_t reg, uint8_t *data, size_t len) {
-  i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-  i2c_master_start(cmd);
-  i2c_master_write_byte(cmd, (device_addr << 1) | I2C_MASTER_WRITE, true);
-  i2c_master_write_byte(cmd, reg, true);
-  i2c_master_start(cmd);
-  i2c_master_write_byte(cmd, (device_addr << 1) | I2C_MASTER_READ, true);
+esp_err_t I2C::reg_read(uint8_t device_addr, uint8_t reg, uint8_t *buffer, size_t len) {
 
-  if (len > 1)
-    i2c_master_read(cmd, data, len - 1, I2C_MASTER_ACK);
-  i2c_master_read_byte(cmd, data + len - 1, I2C_MASTER_NACK);
-  i2c_master_stop(cmd);
+  // get device handle
+  i2c_master_dev_handle_t dev_handle = get_device_handle(device_addr);
 
-  esp_err_t ret = i2c_master_cmd_begin(port, cmd, 1000 / portTICK_PERIOD_MS);
-  i2c_cmd_link_delete(cmd);
+  // check that device handle exists
+  if (!dev_handle) {
+    ESP_LOGE(TAG, "Device 0x%02X not initialized", device_addr);
+    return ESP_ERR_INVALID_STATE;
+  }
+
+  esp_err_t ret =
+      i2c_master_transmit_receive(dev_handle, &reg, sizeof(reg), buffer, len, I2CTIMEOUT_MS);
 
   if (ret != ESP_OK)
     ESP_LOGE(TAG, "I2C read failed from 0x%02X: %s", device_addr, esp_err_to_name(ret));
